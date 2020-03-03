513da07b32239331230861625028bdeb61324ee2
Chady Chaito <chady.chaito@going2.com.br>
2020-03-03 13:15:04 -0300

    Finalizado a parte de Backend, Adicionado Soft Delete no Usuario e validações para Cadastro de Administradores

--- versao-inicial/backend/api/stat.js
+++ versao-inicial/backend/api/stat.js
@@ -2,15 +2,15 @@ module.exports = app => {
     const Stat = app.mongoose.model('Stat', {
         users: Number,
         categories: Number,
-        article: Number,
-        creteadAt: Date
+        articles: Number,
+        createdAt: Date
     })
 
     const get = (req, res) => {
         Stat.findOne({}, {}, { sort: { 'createdAt': -1 } })
             .then(stat => {
                 const defaultStat = {
-                    user: 0,
+                    users: 0,
                     categories: 0,
                     articles: 0
                 }
@@ -19,4 +19,4 @@ module.exports = app => {
     }
 
     return { Stat, get }
-}   
+}
